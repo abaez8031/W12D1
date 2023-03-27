7c0b0bdc7ac14e6a98ad2f785ac37128581573b8
Michael Gastello <michaelgastello@aol.com>
2023-03-27 15:39:26 -0400

    validations

---
+++ app/models/item.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: items
+#
+#  id         :bigint           not null, primary key
+#  pokemon_id :bigint           not null
+#  name       :string           not null
+#  price      :integer          not null
+#  happiness  :integer          not null
+#  image_url  :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Item < ApplicationRecord
+    validates :name, length: { maximum: 255 }
+    validates :price, numericality: { minimum: 0 }
+end
